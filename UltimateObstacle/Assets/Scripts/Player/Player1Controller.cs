ddb53172d27f18611604877a959133bbb8c5bd4c
Benjamin124124 <73146397+Benjamin124124@users.noreply.github.com>
2021-08-06 14:40:35 +1200

    Added Most Obstacles

--- UltimateObstacle/Assets/Scripts/Player/Player1Controller.cs
+++ UltimateObstacle/Assets/Scripts/Player/Player1Controller.cs
@@ -2,14 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player1Controller : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
+
+    public Vector3 jump;
+
     public float speed = 6f;
+    public float jumpForce = 2.0f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public bool isGrounded;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 
     // Update is called once per frame
@@ -18,9 +35,21 @@ public class Player1Controller : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
         if(direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
 }
